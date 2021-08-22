323dcb5840f96a30c016a481205c081ccecdf8d2
etamar <etamar.levy@gmail.com>
2021-08-22 10:45:43 -0400

    commit tree acb823c1e41ddf85408bbcb465dbeff2f48ab23e

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,4 @@
+
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
@@ -15,17 +16,24 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@cart == []
-        resp.write "Your cart is empty"
-      else
-        @@cart.map do |item|
-          resp.write item
+        if @@cart == []
+          resp.write "Your cart is empty"
+        else
+            @@cart.map do |item|
+              resp.write "#{item}\n"
+            end
         end
-      end
+    elsif req.path.match(/add/)
+        search_term = req.params["item"]
+        if @@items.include?(search_term)
+          @@cart << search_term
+          resp.write "added #{search_term}"
+        else
+          resp.write "We don't have that item"
+        end
     else
       resp.write "Path Not Found"
     end
-
     resp.finish
   end
 
